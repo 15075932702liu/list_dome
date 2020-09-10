93ce95d50958799f04a29cdc5f261d5760f51a12
刘同亮 <3132670165@qq.com>
2020-09-10 09:30:01 +0800

    第一次修改

---
+++ list/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <van-collapse v-model="activeNames" class="van">
+      <van-collapse-item title="标题1" name="1">内容</van-collapse-item>
+      <van-collapse-item title="标题2" name="2">内容</van-collapse-item>
+      <van-collapse v-model="activeNames" title="标题33">
+         <van-collapse-item title="标题3" name="3" disabled>内容</van-collapse-item>
+      </van-collapse>
+    </van-collapse>
+  </div>
+</template>
+
+<script>
+
+export default {
+ data() {
+    return {
+      activeNames: ['1'],
+    };
+  },
+};
+</script>
+
+<style>
+   .van{
+     width: 100px;
+     height: 60px;
+   }
+</style>
